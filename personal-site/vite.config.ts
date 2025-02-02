d44a3cdcfedce12c160f55086c6db70b2ad1887a
Jasmine <marwahaj@uci.edu>
2025-02-01 16:40:54 -0800

    gh pages issue

--- personal-site/vite.config.ts
+++ personal-site/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/jasminemarwaha1234.github.io", plugins: [react()],
+  plugins: [react()],
 })
